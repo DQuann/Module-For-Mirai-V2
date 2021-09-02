ac04b1f99a7fa195404b97e09ec345c3b4ea18ac
Nguyễn Duy Quân <69776071+DQuann@users.noreply.github.com>
2021-09-02 21:15:54 +0700

    Update rules.js

--- rules.js
+++ rules.js
@@ -1,4 +1,5 @@
-** Hầu hết mấy ông dùng bypass module donate thì chả cần dùng cái này đâu vì tội gì mà không dùng mấy module xịn hơn đúng k =))**
+/** Hầu hết mấy ông dùng bypass module donate thì chả cần dùng cái này đâu vì tội gì mà không dùng mấy module xịn hơn đúng k =))**/
+/** Đổi Credit ? Nếu bạn không có văn hóa như Hà Mạc Trường Giang ¯\_(ツ)_/¯ **/
 module.exports.config = {
 	name: "rules",
 	version: "1.0.0",
@@ -10,7 +11,7 @@ module.exports.config = {
 };
 
 module.exports.run = ({ event, api }) => api.sendMessage(`Điều khoản sử dụng bot trong box:
-⚠ Vui lòng chấp hàn nghiêm chỉnh để tránh bị hạn chế dùng lệnh ( user ban )
+⚠ Vui lòng chấp hành nghiêm chỉnh để tránh bị hạn chế dùng lệnh ( user ban )
 1: Không spam lệnh bot, spam prefix quá nhiều gây die bot,cp....
 2: Không gây war với bot ( modules sim ... ) vì đây không phải người dùng tương tác thật !
 3: Không lạm dụng bot vào mục đích xấu.,...
