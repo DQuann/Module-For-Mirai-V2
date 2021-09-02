a3db230935acb8106757b1a8686ba98fb4b9431e
Nguyễn Duy Quân <69776071+DQuann@users.noreply.github.com>
2021-09-02 21:12:34 +0700

    Update rules.js

--- rules.js
+++ rules.js
@@ -1,9 +1,10 @@
+** Hầu hết mấy ông dùng bypass module donate thì chả cần dùng cái này đâu vì tội gì mà không dùng mấy module xịn hơn đúng k =))**
 module.exports.config = {
 	name: "rules",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "Mirai Team - NDQ",
-	description: "( Được mod từ module Penis của Mirai Source ) ",
+	credits: "Nguyễn Duy Quân",
+	description: "Thì là luật đó",
 	commandCategory: "info",
 	cooldowns: 1
 };
@@ -13,5 +14,4 @@ module.exports.run = ({ event, api }) => api.sendMessage(`Điều khoản sử d
 1: Không spam lệnh bot, spam prefix quá nhiều gây die bot,cp....
 2: Không gây war với bot ( modules sim ... ) vì đây không phải người dùng tương tác thật !
 3: Không lạm dụng bot vào mục đích xấu.,...
-4: Cập nhật thêm ....
-© Bot Made By Mirai Team `, event.threadID, event.messageID);
+4: Cập nhật thêm ....`, event.threadID, event.messageID);
