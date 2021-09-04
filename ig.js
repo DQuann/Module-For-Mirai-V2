086aee2cd8dadd1301c9a7a256b490804771a7e2
Nguyễn Duy Quân <69776071+DQuann@users.noreply.github.com>
2021-09-04 20:45:31 +0700

    Update ig.js

--- ig.js
+++ ig.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "ig",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "Phan Trần Anh Tâm ~~",
+  credits: "PTAT & NDQ",
   description: "Random ảnh gái trên Intagram",
   commandCategory: "Hình ảnh",
   usages: "ig",
